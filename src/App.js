2d98acff6b742131a0f8a3cb40200cb955b3e0c5
AbirMondal1234 <69684752+AbirMondal1234@users.noreply.github.com>
2024-01-21 17:23:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-
+import { Router } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-     <h1>My Note</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route  path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
